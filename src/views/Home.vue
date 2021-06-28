539612d1e44dc6d8a621379de393f59cee9098e8
devcjlozano <carlosjlp@gmail.com>
2021-06-28 17:07:27 +0200

    initial config and socket connection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,9 +8,19 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import VueSocketIO from "vue-socket.io";
+import Vue from "vue";
 
 export default {
   name: "Home",
+  created() {
+    Vue.use(
+      new VueSocketIO({
+        debug: true,
+        connection: "http://192.168.1.50:3000/",
+      })
+    );
+  },
   components: {
     HelloWorld,
   },
