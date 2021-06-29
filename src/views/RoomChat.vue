f735eb9deef855899684c984fe5bf89c67313ed9
devcjlozano <carlosjlp@gmail.com>
2021-06-29 17:00:20 +0200

    add RoomChat component and router

---
+++ src/views/RoomChat.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="room-chat">
+    <h3>Sala de chat</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RoomChat",
+  created() {
+    if (!this.$route.params.nombreSala) {
+      this.$router.push({
+        name: "Home",
+      });
+    }
+  },
+};
+</script>
