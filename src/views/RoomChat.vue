8d572f58ec3785d3b33abeccf9eebfbedb5c89ec
devcjlozano <carlosjlp@gmail.com>
2021-07-01 18:17:22 +0200

    Added style component RoomChat

--- src/views/RoomChat.vue
+++ src/views/RoomChat.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="room-chat">
-    <h3>Sala de chat</h3>
+    <h1>Chat sistema cj</h1>
+    <div class="room-chat__box">
+      <div class="room-chat__box__users">
+        <div
+          v-for="user in users"
+          :key="user.id"
+          class="room-chat__box__users_user"
+        >
+          <div class="avatar" />
+          <span class="user_name"> {{ user.name }} </span>
+        </div>
+      </div>
+
+      <div class="room-chat__box__main">
+        <div class="room-chat__box__main__header">
+          <span class="room-chat__box__main__header-title"> Sala de chat </span>
+          <span class="room-chat__box__main__header-room-text">
+            {{ nameRoom }}
+          </span>
+        </div>
+        <div class="room-chat__box__main__messages"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -61,3 +83,61 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.room-chat {
+  background: #f4f6f9;
+  height: 100%;
+  padding: 20px;
+}
+.room-chat__box {
+  position: relative;
+  background: #fff;
+  display: flex;
+  width: 100%;
+  height: 80%;
+  border: 1px solid #eeeff0;
+}
+.room-chat__box__users {
+  width: 200px;
+  height: 100%;
+  border: 1px solid #eeeff0;
+  overflow-y: auto;
+}
+.room-chat__box__users_user {
+  display: flex;
+  align-items: center;
+  padding: 5px;
+}
+.room-chat__box__main {
+  width: 100%;
+}
+.room-chat__box__main__header {
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px solid #eeeff0;
+}
+.room-chat__box__main__header-title {
+  font-size: 1.4em;
+  margin-right: 10px;
+}
+.room-chat__box__main__header-room-text {
+  white-space: nowrap;
+  font-size: 1.1em;
+}
+.user_name {
+  white-space: nowrap;
+}
+.avatar {
+  background-image: url("http://i.stack.imgur.com/Dj7eP.jpg");
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+  background-size: cover;
+  background-position: top center;
+  border-radius: 50%;
+}
+</style>
